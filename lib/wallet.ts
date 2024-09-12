e936802549720e2f37e5fddb7e5ffc03536922ce
DavNej <david.nejar@gmail.com>
2024-09-12 14:46:22 +0200

    feat: wallet creation. generate mnemonic + payable BTC address

---
+++ lib/wallet.ts
@@ -0,0 +1,21 @@
+import { BIP32Factory } from 'bip32'
+import * as bip39 from 'bip39'
+import * as bitcoin from 'bitcoinjs-lib'
+import * as ecc from 'tiny-secp256k1'
+
+export function generateHDWallet() {
+  // Generate a mnemonic (BIP39)
+  const mnemonic = bip39.generateMnemonic()
+  const seed = bip39.mnemonicToSeedSync(mnemonic)
+
+  const root = BIP32Factory(ecc).fromSeed(seed)
+
+  // Derive the first Bitcoin address to receive funds (BIP44)
+  const path = "m/44'/0'/0'/0/0"
+  const child = root.derivePath(path)
+
+  // Get the address from the child node
+  const { address } = bitcoin.payments.p2pkh({ pubkey: child.publicKey })
+
+  return { mnemonic, address }
+}
