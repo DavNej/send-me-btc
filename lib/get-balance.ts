20cb6d30d97563c72b6f2fac8de829292c2b24c2
DavNej <david.nejar@gmail.com>
2024-09-18 12:38:56 +0200

    WIP

--- lib/get-balance.ts
+++ lib/get-balance.ts
@@ -13,14 +13,18 @@ export function bitcoinBalanceOptions({
     placeholderData: { balance: 0 },
     queryFn: async () => {
       try {
-        const response = await fetch(
-          `https://api.blockcypher.com/v1/btc/test3/addrs/${address}/balance`
-        )
-        return response.json()
+        // const response = await fetch(
+        //   `https://api.blockcypher.com/v1/btc/test3/addrs/${address}/balance`
+        // )
+        // return response.json()
+        const response = await fetch('https://catfact.ninja/fact')
+        const d = await response.json()
+        return Promise.resolve({ balance: d.length })
       } catch (error) {
         throw new Error('Failed to fetch balance: ' + (error as Error).message)
       }
     },
-    refetchInterval: enabled ? 60 * 1000 : false, // Refetch every 1 minute if query is enabled
+    refetchInterval: enabled ? 4 * 1000 : false, // Refetch every 1 minute if query is enabled
+    // 60 * 1000
   })
 }
